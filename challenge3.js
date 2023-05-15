2b7bde49c5014b8cd13a9389abe773345d35576b
Lindokuhle Shweni <127991096+Shweni@users.noreply.github.com>
2023-05-15 23:01:02 +0200

    Add files via upload

---
+++ challenge3.js
@@ -0,0 +1,24 @@
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+
+const divider = '----------------------------------'
+
+// Only change below this line
+
+const owed ="R"+ leoBalance+ sarahBalance
+const leo = leoName + leoSurname
+const sarah = leoName + leoSurname  
+const total = "total amount owed:"
+const result = leoName + leoSurname + leoBalance + sarahName + sarahSurname +  sarahBalance + divider + total + owed + divider 
+
+ 
+console.log(leoName + leoSurname + "R" + leoBalance )
+console.log(sarahName + sarahSurname + "R" + sarahBalance)
+console.log(divider)
+console.log(owed)
+console.log(divider)
